ea0b9ed441a743bc880d5064735ac9370dd28281
Thomas A. McGonagle <mcgonagle@gmail.com>
2016-10-31 15:10:17 -0400

    fixed disabled

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 touch /tmp/file_terraform_test
+tmsh modify sys global-settings gui-setup disabled
+tmsh modify sys global-settings hostname f5-bigip.f5customer.com
+tmsh modify sys dns name-servers add { 8.8.4.4 }
+tmsh modify sys ntp timezone America/New_York servers add { time.apple.com }
 tmsh create net vlan external interfaces add { 1.1 { untagged } }
 tmsh create net vlan internal interfaces add { 1.2 { untagged } }
 tmsh create net self 10.0.1.10 address 10.0.1.10/24 vlan external
