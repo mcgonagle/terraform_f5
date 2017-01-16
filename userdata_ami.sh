6d7db4ed909c4254c77f34a20b3f012367975445
Thomas A. McGonagle <t.mcgonagle@f5.com>
2017-01-16 14:52:09 -0500

    Added bodgeit webstore to userdata file

--- userdata_ami.sh
+++ userdata_ami.sh
@@ -17,3 +17,13 @@ cd /home/ec2-user
 /usr/bin/wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
 
 sudo service httpd start
+
+sudo yum install -y tomcat8
+
+cd /usr/share/tomcat8/webapps
+
+sudo /usr/bin/wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/bodgeit/bodgeit.1.4.0.zip
+
+sudo unzip bodgeit.1.4.0.zip
+
+sudo service tomcat8 start
